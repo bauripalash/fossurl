001cf669853e54aae443eb495be938a257bf3cbb
Palash Bauri <palashbauri1@gmail.com>
2018-06-15 10:31:35 +0530

    working copy

---
+++ index.js
@@ -0,0 +1,65 @@
+var urldb = "https://www.jsonstore.io/79a85a647a95f875559fd3683f975e61b120279d8dcfd0e7e73e764979b3332e";
+// var urllist = "https://www.jsonstore.io/634de8a8aa3d48c3074c5a01ec605368cd11248502306ee7a4916ad67d2222b0";
+
+function genran(){
+
+    if(window.location.hash == ""){
+        window.location.hash =Math.random().toString(32).substring(2, 5) + Math.random().toString(36).substring(2, 5);
+        // $.getJSON(urllist + "/" + window.location.hash, function(data){
+        //     data = data["result"];
+        //     if(data!=null){
+        //         window.location.hash =Math.random().toString(32).substring(2, 5) + Math.random().toString(36).substring(2, 5);
+        //     }
+            
+        // });
+        
+    }
+
+}
+
+$("#sbtn").click(shorten);
+// document.getElementById("sbtn").onclick = shorten;
+function shorten(){
+    var urlb = document.getElementById("urlbox").value;
+    genran();
+    short_url(urlb);
+    console.log("shorten");
+    
+}
+
+function short_url(url){
+    this.url = url;
+    $.ajax({
+        'url': urldb + "/" + window.location.hash.substr(1),
+        'type': 'POST',
+        'data': JSON.stringify(this.url),
+        'dataType': 'json',
+        'contentType': 'application/json; charset=utf-8'
+      })
+
+    //   $.ajax({
+    //     'url': urllist + "/" + "urllist997695",
+    //     'type': 'PUT',
+    //     'data': JSON.stringify(this.url),
+    //     'dataType': 'json',
+    //     'contentType': 'application/json; charset=utf-8'
+    //   })
+    // console.log("short_url");
+    simplecopy(window.location.href);
+}
+var hashh = window.location.hash.substr(1)
+
+
+if(window.location.hash != ""){
+$.getJSON(urldb + "/" + hashh, function(data){
+  data = data["result"];
+  
+  if(data!=null){
+      window.location.href = data;
+  }
+  
+});
+}
+
+
+
