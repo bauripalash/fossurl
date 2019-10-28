d67236bda53c336a442effa947ed96501abbf372
Palash Bauri <palashbauri1@gmail.com>
2019-10-28 15:14:12 +0530

    modifed runme.py for jsonbox

--- src/runme.py
+++ src/runme.py
@@ -3,7 +3,7 @@ import uuid
 
 JSONBOX_WEBSITE = "https://jsonbox.io/"
 
-endpoint_source = '''var endpoint = "%s";function fetchJSON(a){var e=new XMLHttpRequest;e.open("GET",a,false);e.send(null);return e.responseText}var hashh=window.location.hash.substr(1);if(window.location.hash!=""){var res=JSON.parse(fetchJSON(endpoint+"/"+hashh));var data=res.result;if(data!=null){window.location.href=data}}
+endpoint_source = '''var endpoint="%s";function fetchJSON(a){var f=new XMLHttpRequest;f.open("GET",a,false);f.send(null);return f.responseText}function isURL(a){var f=/^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;if(f.test(a)){console.log(f.test(a));return true}else{console.log(f.test(a));return false}}var hashh=window.location.hash.substr(1);if(window.location.hash!=""){var res=JSON.parse(fetchJSON(endpoint+"/?q=s:"+hashh))[0];var data=res["l"];if(data!=null){if(isURL(data)){window.location.href=data}}}
 '''
 
 info_yml = "endpoint: %s \nendpoint_dashboard: %s"
@@ -19,7 +19,7 @@ def main():
     print("Your JSONBOX Endpoint is : " + JSONBOX_WEBSITE +  endpoint);
     print("Your JSONBOX Dashboard URL is : " + JSONBOX_WEBSITE + "dashboard.html?box=" + endpoint )
 
-    with open("header.js" , "w") as f:
+    with open("head.js" , "w") as f:
         f.write(endpoint_source %(JSONBOX_WEBSITE +  endpoint))
 
     with open("DELETE_ME.yml" , "w") as f:
